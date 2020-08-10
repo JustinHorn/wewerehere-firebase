b07487fcac750cd24273a9450303f5542406b9ef
y0092888 <ju-horn@web.de>
2020-08-10 15:37:21 +0200

    Updates

---
+++ precache-manifest.9a6f253a82de05012a3982eb0197ade4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef36a0b44d2de56506101cfb587b518f",
+    "url": "/wewerehere-firebase/index.html"
+  },
+  {
+    "revision": "edc2adc1381c3aa0b123",
+    "url": "/wewerehere-firebase/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f08e547703a6115720c7",
+    "url": "/wewerehere-firebase/static/js/2.c14a6083.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/wewerehere-firebase/static/js/2.c14a6083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc2adc1381c3aa0b123",
+    "url": "/wewerehere-firebase/static/js/main.bdac4bb4.chunk.js"
+  },
+  {
+    "revision": "03693fbd2e034ce81bbe",
+    "url": "/wewerehere-firebase/static/js/runtime-main.fd3ea81c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/justinhorn.name/wewerehere-firebase/precache-manifest.5cd2e0d4c9928f778a2e9a8556c975ca.js"
+  "/wewerehere-firebase/precache-manifest.9a6f253a82de05012a3982eb0197ade4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/justinhorn.name/wewerehere-firebase/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wewerehere-firebase/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
