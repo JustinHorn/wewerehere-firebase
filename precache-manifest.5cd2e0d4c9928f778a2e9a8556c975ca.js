956e20e2a2a5a1cd25f8d05d5f68b11e272f00b2
y0092888 <ju-horn@web.de>
2020-08-10 15:34:59 +0200

    Updates

---
+++ precache-manifest.5cd2e0d4c9928f778a2e9a8556c975ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdae3df477a0418837d6e4ac7691b31",
+    "url": "/justinhorn.name/wewerehere-firebase/index.html"
+  },
+  {
+    "revision": "edc2adc1381c3aa0b123",
+    "url": "/justinhorn.name/wewerehere-firebase/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "becfe8d7bc0cd46d3f15",
+    "url": "/justinhorn.name/wewerehere-firebase/static/js/2.3d6d6aa4.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/justinhorn.name/wewerehere-firebase/static/js/2.3d6d6aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc2adc1381c3aa0b123",
+    "url": "/justinhorn.name/wewerehere-firebase/static/js/main.bdac4bb4.chunk.js"
+  },
+  {
+    "revision": "751707b3822e9da84fc5",
+    "url": "/justinhorn.name/wewerehere-firebase/static/js/runtime-main.44e1892b.js"
+  }
+]);
